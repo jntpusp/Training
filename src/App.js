349b4f8625b44c188c1eee146b688c3be80bb544
Jayanti Pusp <jayantipusp@gmail.com>
2020-02-10 16:11:01 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Form, Row, Label, Button } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h2>Sample Form</h2>
       </header>
+      <Form class="form-inline">
+        <Label for="email" class="mr-sm-2">
+          Email address:
+        </Label>
+        <input
+          type="email"
+          class="form-control mb-2 mr-sm-2"
+          placeholder="Enter email"
+          id="email"
+        />
+        <Label for="pwd" class="mr-sm-2">
+          Password:
+        </Label>
+        <input
+          type="password"
+          class="form-control mb-2 mr-sm-2"
+          placeholder="Enter password"
+          id="pwd"
+        />
+        <div class="form-check mb-2 mr-sm-2">
+          <Label class="form-check-label">
+            <input class="form-check-input" type="checkbox" /> Remember me
+          </Label>
+        </div>
+        <Button type="submit" class="btn btn-primary mb-2">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
